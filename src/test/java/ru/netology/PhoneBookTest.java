ec069a6b20323ce87fdf7cb92b95e94cb112b2a9
Nika_Miku <diademistv@gmail.com>
2024-03-30 04:08:59 +0300

    Stub and tests for add.

--- src/test/java/ru/netology/PhoneBookTest.java
+++ src/test/java/ru/netology/PhoneBookTest.java
@@ -1,8 +1,13 @@
 package ru.netology;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PhoneBookTest {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    @Test
+    public void testAddReturnsZero() {
+        PhoneBook phoneBook = new PhoneBook();
+        int result = phoneBook.add();
+        assertEquals(1, result); // Тест не должен проходить
     }
 }
