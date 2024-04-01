73cdeda07fb6389778c99235ac0fc660921b6105
Nika_Miku <diademistv@gmail.com>
2024-04-01 23:12:54 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,7 +1,25 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
 public class PhoneBook {
-    public int add (){
-        return 0;
+    private HashMap<String, String> contacts;
+
+    public PhoneBook() {
+        this.contacts = new HashMap<>();
+    }
+
+    public int add(String phone, String name) {
+        if (Pattern.matches("79\\d{9}", phone) && Pattern.matches("^[A-Za-z]+([-][A-Za-z]+)*$", name)) {
+            contacts.put(phone, name);
+            return contacts.size();
+        } else {
+            return 0;
+        }
+    }
+
+    public HashMap<String, String> getContacts() {
+        return contacts;
     }
 }
--- src/test/java/ru/netology/PhoneBookTest.java
+++ src/test/java/ru/netology/PhoneBookTest.java
@@ -1,13 +1,35 @@
 package ru.netology;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PhoneBookTest {
 
     @Test
-    public void testAddReturnsZero() {
+    public void testAddReturnsPhone() {
         PhoneBook phoneBook = new PhoneBook();
-        int result = phoneBook.add();
-        assertEquals(1, result); // Тест не должен проходить
+        String input = "79776776767";
+        boolean isMatching = Pattern.matches("79\\d{9}", input);
+        assertTrue(isMatching, "Номер телефона должен соответствовать шаблону");
+    }
+
+    @Test
+    public void testAddReturnsName() {
+        PhoneBook phoneBook = new PhoneBook();
+        String input = "Ayaz";
+        boolean isMatching = Pattern.matches("^[A-Za-z]+([-][A-Za-z]+)*$", input);
+        assertTrue(isMatching, "Имя должно соответствовать шаблону");
+    }
+
+    @Test
+    public void testAddReturnsContacts() {
+        PhoneBook phoneBook = new PhoneBook();
+        HashMap<String, String> contacts = new HashMap<>();
+        contacts.put("79776776767", "Yasha");
+        int result = phoneBook.add("79999191880", "Ayaz");
+        assertNotNull(result, "Список контактов не должен быть пустым");
     }
 }
